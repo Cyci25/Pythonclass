2e0890a3ba8a795146567adc924c33ac7cec606c
Cyci25 <c2muriithi@gmail.com>
2022-10-04 10:27:32 +0300

    Daily revison

---
+++ helloworld.py
@@ -0,0 +1,280 @@
+import random
+print("Hello World!")
+
+if 5 > 2:
+    print("Five is greater than two!")
+    # print("Five is greater than two!")
+#    Commening
+"""
+    please bare it
+    it is not my problem
+    """
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+x = 5
+y = "Jane"
+print(y)
+print(x)
+
+x = 40
+x = "Kim"
+print(x)
+
+# Casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x)
+print(y)
+print(z)
+
+# Type of Variable
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# Single or double Quote
+x = 'Joan'
+y = "Jon"
+print(x)
+print(y)
+if x == y:
+    print("The name is Joan")
+
+# Case sensitive
+a = 4
+A = "Sally"
+# A will not overwrite a. Python is case sensitive
+print(a)
+print(A)
+
+# Variable naming
+myvar = "James"
+my_var = "James"
+MyVar = "James"
+_my_var = "James"
+myVar = "James"
+myVAR = "James"
+myvar2 = "James"
+MYVAR = "James"
+
+print(myvar)
+print(my_var)
+print(MyVar)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+print(myVAR)
+
+# Variable Casing
+# Camel Case
+myVariableName = "Joan"
+print(myVariableName)
+# Pascal Case
+MyVariableName = "Joan"
+print(MyVariableName)
+# Snake Case
+my_variable_name = "Joan"
+print(my_variable_name)
+
+# Assigning multiple values
+x, y, z = 'Joan', 'Cynthia', 'Kuria'
+print(x)
+print(y)
+print(z)
+
+# Assigning one value to multiple variables
+x = y = z = "Cynthia"
+print(x)
+print(y)
+print(z)
+
+# Unpackin
+fruits = ['apples', 'bananas', 'cherries']
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+# print function
+x = ("Python is amazing")
+print(x)
+
+# mulitple outputs
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# + operator without space (Pythonisamaizing)
+x = "Python"
+y = "is"
+z = "amaizing"
+print(x + y + z)
+
+# + operator with space (Python is amaizing)
+x = "Python "
+y = "is "
+z = "amaizing"
+print(x + y + z)
+
+# + operator with interger
+x = 5
+y = 8
+print(x+y)
+
+# Global Variables
+x = "amaizing"
+
+
+def myfunc():
+    print("Cynthia is " + x)
+
+
+myfunc()
+
+# Global variables and local variables
+x = "awesome"
+
+
+def myfun():
+    x = "fantastic"
+    print("Python is " + x)
+
+
+myfun()
+
+print("Python is " + x)
+
+# Global Keyword (Local keyword used globally)
+
+
+def myfunction():
+    global x
+    x = "amaizing"
+
+
+myfunction()
+
+print("Cynthia is really " + x)
+
+# Defining Data Types
+# String
+x = str('Jane')
+y = ("Jane")
+print(x, y)
+print(type(x))
+print(type(y))
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# integers
+x = 5
+y = int(5)
+print(x, y)
+print(type(x))
+print(type(y))
+
+# float
+x = 3.5
+y = float(3.5)
+print(x, y)
+print(type(x))
+print(type(y))
+
+# complex
+x = 1j
+y = complex(1j)
+print(x, y)
+print(type(x))
+print(type(y))
+
+# list
+x = ["Joan", "Cynthia", "Kuria"]
+y = list(("Joan", "Cynthia", "Kuria"))
+print(x, y)
+print(type(x))
+print(type(y))
+
+# tuple
+x = ("Joan", "Cynthia", "Kuria")
+y = tuple(("Joan", "Cynthia", "Kuria"))
+print(x, y)
+print(type(x))
+print(type(y))
+
+# range
+x = range(6)
+y = range(6)
+print(x, y)
+print(type(x))
+print(type(y))
+
+# Dictionary
+x = {"Name: ": "Jack", "Age: ": 30}
+y = dict(Name="John", Age=30)
+print(x, y)
+print(type(x))
+print(type(y))
+
+# Sets
+x = {"Joan", "Cynthia", "Kuria"}
+y = set(("Joan", "Cynthia", "Kuria"))
+print(x, y)
+print(type(x))
+print(type(y))
+
+# frozenset
+x = frozenset({"Joan", "Cynthia", "Kuria"})
+y = frozenset(("Joan", "Cynthia", "Kuria"))
+print(x, y)
+print(type(x))
+print(type(y))
+
+# Boolean (True or False)
+x = True
+y = bool(5)
+print(x, y)
+print(type(x))
+print(type(y))
+
+# Bytes
+x = b"Hello"
+y = bytes(5)
+print(x, y)
+print(type(x))
+print(type(y))
+
+# bytearray
+x = bytearray(5)
+y = bytearray(5)
+print(x, y)
+print(type(x))
+print(type(y))
+
+# memoryview
+x = memoryview(bytes(5))
+y = memoryview(bytes(5))
+print(x, y)
+print(type(x))
+print(type(y))
+
+# Nonetype
+x = None
+print(x)
+print(type(x))
+
+# Print a random number in the range of 1-10
+
+print(random.randrange(1, 10))
---
+++ helloworld2.py
@@ -0,0 +1,144 @@
+a = "Hello, World!"
+print(a[3])
+
+for x in "banana":
+    print(x)
+
+a = "Hey you!"
+print(len(a))
+
+a = "'The best things in life are free!"
+print("free" in a)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+    print('Yes, "free" is in txt.')
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+    print("'Expensive' is not in txt")
+
+
+def myfunc():
+
+    a = "Cynthia"
+    print(a)
+
+myfunc()
+
+# slicing 
+a = "Hello World!"
+print(a[2:6])
+
+# Slice from start 
+a = "Hello World!"
+print(a[:7])
+
+# Slice to end 
+a = "Hello World!"
+print(a[2:])
+
+# slice negative 
+b = "Hello, World!"
+print(b[-5:-2])
+
+# Uppercase
+a= "Hey you!"
+print(a.upper())
+
+# Lowercase
+a= " Hey you! "
+print(a.lower())
+
+# Stip
+a= " Hey you! "
+print(a.strip())
+
+#capitalize
+a= "hey you! " + "i don't really like you. " + "have a great day!"
+print(a.capitalize())
+
+# replace 
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+# Split 
+a = "Hello, World! I am, Cynthia"
+print(a.split(",")) 
+
+# Concatiation 
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+# insert numbers into strings 
+age = 20
+txt = "I am {}"
+txt2 = " years old"
+print(txt.format(age) + txt2)
+
+age = 65
+txt = "I am {} years old"
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars. Plus {} Change"
+print(myorder.format(quantity, itemno, price, price))
+
+# escape Character
+txt = "The best things in life are \"free\"!"
+print(txt)
+
+txt = "It's alright."
+print(txt)
+
+txt = 'It\'s alright.'
+print(txt)
+
+txt = "This will insert one \\ (backslash)."
+print(txt) 
+
+# Insert on line (br)
+txt = "Hello\nWorld!"
+print(txt) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
